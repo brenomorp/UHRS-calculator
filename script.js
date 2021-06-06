d51a3d7d911b1b074c8e72f41558eb757b1f05fc
Breno Moreira <brenomoreirapro@gmail.com>
2021-06-06 04:22:42 -0300

    0.0

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculate() {
+    var money = document.getElementById('money')
+    var time = document.getElementById('time')
+    var res = document.getElementById('res')
+
+
+    if (money.value.length == 0 || time.value.length == 0) {
+        window.alert('Error')
+    } else {
+        var n1 = 3600 / Number(time.value)
+        var MoneyReplace = parseFloat(money.value.replace(",", "."))
+        var n2 = n1 * MoneyReplace
+
+
+        res.innerHTML = `
+<p><strong>${n1}</strong> hits per hour</p>
+<p><strong>$${n2}</strong> per hour</p>
+`
+    }
+}
